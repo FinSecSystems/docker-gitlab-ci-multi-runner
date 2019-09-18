2df58e388a0d62299e98976b88b33f8028aa1503
Liquibrium Admin <admin@liquibrium.com>
2019-09-18 16:33:40 +0000

    Added dns

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ CA_CERTIFICATES_PATH=${CA_CERTIFICATES_PATH:-$GITLAB_CI_MULTI_RUNNER_DATA_DIR/ce
 create_data_dir() {
   mkdir -p ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}
   chown ${GITLAB_CI_MULTI_RUNNER_USER}:${GITLAB_CI_MULTI_RUNNER_USER} ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}
+  mkdir -p ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/tmp/
+  chown ${GITLAB_CI_MULTI_RUNNER_USER}:${GITLAB_CI_MULTI_RUNNER_USER} ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/tmp/
+  mkdir -p ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.conan/
+  chown ${GITLAB_CI_MULTI_RUNNER_USER}:${GITLAB_CI_MULTI_RUNNER_USER} ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.conan/
 }
 
 generate_ssh_deploy_keys() {
@@ -17,10 +21,10 @@ generate_ssh_deploy_keys() {
     sudo -HEu ${GITLAB_CI_MULTI_RUNNER_USER} ssh-keygen -t rsa -N "" -f ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa
 
     echo ""
-    echo -n "Your SSH deploy key is: "
-    cat ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa.pub
-    echo ""
   fi
+  echo -n "Your SSH deploy key is: "
+  cat ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa.pub
+  echo ""
 
   chmod 600 ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh/id_rsa.pub
   chmod 700 ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/.ssh
@@ -52,7 +56,7 @@ configure_ci_runner() {
     if [[ -n ${CI_SERVER_URL} && -n ${RUNNER_TOKEN} && -n ${RUNNER_DESCRIPTION} && -n ${RUNNER_EXECUTOR} && -n ${CI_CLONE_URL} && -n ${RUNNER_NETWORK} ]]; then
       sudo -HEu ${GITLAB_CI_MULTI_RUNNER_USER} \
         gitlab-runner register --config ${GITLAB_CI_MULTI_RUNNER_DATA_DIR}/config.toml \
-	-n -u "${CI_SERVER_URL}" --clone-url "${CI_CLONE_URL}" -r "${RUNNER_TOKEN}" --name "${RUNNER_DESCRIPTION}" --executor "${RUNNER_EXECUTOR}" --docker-image "docker:stable" --docker-volumes /var/run/docker.sock:/var/run/docker.sock --docker-volumes ${RUNNER_BUILD_DIR}/.ssh:/root/.ssh --docker-volumes ${RUNNER_BUILD_DIR}/builds:/builds --docker-network-mode "${RUNNER_NETWORK}" --env RUNNER_BUILD_DIR=${RUNNER_BUILD_DIR} --env "CONAN_DOCKER_RUN_OPTIONS=-v ${RUNNER_BUILD_DIR}\$CI_PROJECT_DIR:/home/conan/project -v ${RUNNER_BUILD_DIR}/.ssh:/home/conan/.ssh -v ${RUNNER_BUILD_DIR}/tmp:/tmp -v ${RUNNER_BUILD_DIR}/.conan:/home/conan/.conan" --env "CONAN_DOCKER_HOME=/home" --output-limit "20480"
+	-n -u "${CI_SERVER_URL}" --clone-url "${CI_CLONE_URL}" -r "${RUNNER_TOKEN}" --name "${RUNNER_DESCRIPTION}" --executor "${RUNNER_EXECUTOR}" --docker-image "docker:stable" --docker-volumes /var/run/docker.sock:/var/run/docker.sock --docker-volumes ${RUNNER_BUILD_DIR}/.ssh:/root/.ssh --docker-volumes ${RUNNER_BUILD_DIR}/builds:/builds --docker-network-mode "${RUNNER_NETWORK}" --docker-dns ${DNS_IP} --env RUNNER_BUILD_DIR=${RUNNER_BUILD_DIR} --env "CONAN_DOCKER_RUN_OPTIONS=-v ${RUNNER_BUILD_DIR}\$CI_PROJECT_DIR:/home/conan/project -v ${RUNNER_BUILD_DIR}/.ssh:/home/conan/.ssh -v ${RUNNER_BUILD_DIR}/tmp:/tmp -v ${RUNNER_BUILD_DIR}/.conan:/home/conan/.conan --dns=${DNS_IP}" --env "CONAN_DOCKER_HOME=/home" --output-limit "20480"
       echo "Running on ${RUNNER_NETWORK}..."
     else
       sudo -HEu ${GITLAB_CI_MULTI_RUNNER_USER} \
